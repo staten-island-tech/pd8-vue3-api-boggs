3ca339a3bd9f8b48fc4afe15497c593d4b5eaac1
ericl2055 <eric_lin2055@icloud.com>
2023-05-02 14:00:03 -0400

    Update HomeView.vue

--- nyc/src/views/HomeView.vue
+++ nyc/src/views/HomeView.vue
@@ -2,5 +2,16 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+const accidents = ref('')
+async function getAccidents() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/nc67-uf89.json')
+  let data = await res.json()
+  accidents.value = data.results
+}
+onMounted(() => {
+  getAccidents()
+})
+</script>
 <style></style>
